204bef8741e4f80a99073ee2bd4a9ef312183d2a
yoonkh0503 <qlswlsh34@naver.com>
2021-09-17 02:34:59 +0900

    Summary : 2021.09.17 신호등 레이싱 게임 ------------------------------------------------------------------------ Description - type : Feat - 게임 첫 시작 시 게임에 대한 설명을 해주는 paintGameHelpMsg 함수 구현. - 부스트 사용 시 부스트 On이 되었다는 메시지가 출력되는 기능 구현. ------------------------------------------------------------------------ - type : Fix - Shift 키를 누르면 부스트 모드가 되는데, 이때 단순히 this.speed * 2 로 처리를 해주고 있음. 이렇게 되면 shift키를 계속 누르면 속도가 끝없이 증가하기 때문에 CAR_MAX_SPEED , CAR_DEFAULT_SPEED 변수를 만들어서 사용함. 부스트가 되면 CAR_MAX_SPEED 만큼 증가할 수 없도록, 부스트 모드가 끝나면 CAR_DEFAULT_SPEED 로 돌아오도록 수정함. - 게임 시작 시 도움말을 보여주는 기능을 추가하였는데, 같은 css 클래스를 사용하다보니 게임 종료 모달창도 도움말 모달로 노출되는 문제가 발생함. 해당 문제 수정을 위해 도움말 모달, 도움말 콘텐트, 도움말 모달 게임시작 버튼, 게임 종료 모달, 게임종료 후 재시작 버튼 Element에 id를 부여함.

--- 2021-09-12/js/trafficLight.js
+++ 2021-09-12/js/trafficLight.js
@@ -1,3 +1,26 @@
+const CAR_DEFAULT_SPEED = 20;
+const CAR_MAX_SPEED = 40;
+const container = document.querySelector(".container");
+
+const paintGameHelpMsg = (function() {
+    const helpContentChildren = document.querySelector("#startContent").children;
+    let count = 0;
+    const intervalId = setInterval(() => {
+        helpContentChildren[count].classList.remove("hidden");
+        count++;
+        if(count > 4) {
+            clearInterval(intervalId);
+        }
+    }, 2000);
+
+    const startModal = document.querySelector("#startModal");
+    const gameStartBtn = document.querySelector("#startButton");
+    gameStartBtn.addEventListener('click', function() {
+        container.classList.remove("hidden");
+        startModal.classList.add("hidden");
+    })
+})();
+
 class TrafficLight {
     constructor(color) {
         this.colorTag = document.querySelector(`.${color}`)
@@ -48,7 +71,6 @@ const removePX = (str) => {
     return str || 0;
 };
 
-
 class Car {
     constructor(name, speed) {
         this.name = name;
@@ -63,13 +85,20 @@ class Car {
         return car.style.left = (left + position) + "px";
     };
     
-    boostOn = () => this.speed = this.speed * 2;
+    boostOn = () => {
+        if(this.speed < CAR_MAX_SPEED)
+        {
+            this.speed = CAR_DEFAULT_SPEED * 2;
+        }
 
+        return this.speed;
+    };
+    
     // boostOn() {
     //     this.speed = this.speed * 2;
     // }
 
-    boostOff = () => this.speed = 20;
+    boostOff = () => this.speed = CAR_DEFAULT_SPEED;
 
     // boostOff() {
     //     this.speed = this.speed / 2;
@@ -78,29 +107,34 @@ class Car {
     // this 가 Window를 가리키는 경우는 뭐지?
 };
 
-const car = new Car("ferrari", 20);
-const modal = document.querySelector(".modal");
-const container = document.querySelector(".container");
-const reStartBtn = document.querySelector(".startButton");
+const car = new Car("ferrari", CAR_DEFAULT_SPEED);
+const modal = document.querySelector("#endModal");
+
+const reStartBtn = document.querySelector("#reStartBtn");
 const carImg = document.querySelector("#car");
+const body = document.querySelector("body");
 
 window.addEventListener('keydown', function(target) {
-    const RED_LIGHT_CHECK = light.colorTag.className.includes('red');
-    const FINISH_LINE_CHECK = document.querySelector(".finish2").offsetLeft < carImg.offsetLeft
+    const isRedLight = light.colorTag.className.includes('red');
+    const isFinish = document.querySelector(".finish2").offsetLeft < carImg.offsetLeft
     if(target.ctrlKey === true) {
         if(modal.classList.contains('hidden')) {
             car.move();
         }
         // console.log(light.colorTag.className.includes('red')); // 빨간불인 상태.
 
-        if(RED_LIGHT_CHECK || FINISH_LINE_CHECK) {
+        if(isRedLight || isFinish) {
             gameOver();
         }
     }
 
     if(target.shiftKey === true) {
         car.boostOn();
+        boostNotice.classList.remove("hidden");
         setTimeout(car.boostOff, 2000);
+        setTimeout(function() {
+            boostNotice.classList.add("hidden");
+        } , 2000);
     }
 });
 
@@ -117,14 +151,42 @@ const gameRestart = function() {
 
 reStartBtn.addEventListener('click', gameRestart);
 
+const boostNotice = document.createElement("h1");
+boostNotice.textContent = "부스트 ON!"
+body.appendChild(boostNotice);
+boostNotice.classList.add("hidden");
+
 
 /* To Do
 - 빨간불인 상태일 때 이동하게 되면 게임 종료되는 기능 구현해야함.
 - 자동차가 finish Line을 넘으면 게임이 종료되는 기능 구현해야함.
 - 게임설명 및 난이도를 고를 수 있도록 모달 추가해야함.
 - 재시작 기능 구현해야함.
-
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* 
 Summary : 2021.09.14 신호등 레이싱 게임
@@ -145,3 +207,23 @@ To Do List
 - 게임설명 및 난이도를 선택할 수 있는 모달 창 추가
 - 부스트 기능 개선이 필요함. 현재 shift키를 누를때마다 스피드가 증가하는 문제가 있음.
 setter와 getter가 필요할지 고려해야함.
+*/
+
+/*
+Summary : 2021.09.17 신호등 레이싱 게임
+------------------------------------------------------------------------
+Description
+- type : Feat
+- 게임 첫 시작 시 게임에 대한 설명을 해주는 paintGameHelpMsg 함수 구현.
+- 부스트 사용 시 부스트 On이 되었다는 메시지가 출력되는 기능 구현.
+------------------------------------------------------------------------
+- type : Fix
+- Shift 키를 누르면 부스트 모드가 되는데, 이때 단순히 this.speed * 2 로 처리를 해주고 있음.
+이렇게 되면 shift키를 계속 누르면 속도가 끝없이 증가하기 때문에 CAR_MAX_SPEED , CAR_DEFAULT_SPEED
+변수를 만들어서 사용함. 부스트가 되면 CAR_MAX_SPEED 만큼 증가할 수 없도록, 부스트 모드가 끝나면
+CAR_DEFAULT_SPEED 로 돌아오도록 수정함.
+- 게임 시작 시 도움말을 보여주는 기능을 추가하였는데, 같은 css 클래스를 사용하다보니 게임 종료 모달창도 도움말 모달로
+노출되는 문제가 발생함.
+해당 문제 수정을 위해 도움말 모달, 도움말 콘텐트, 도움말 모달 게임시작 버튼, 게임 종료 모달, 게임종료 후 재시작 버튼 Element에
+id를 부여함.
+*/
